24156a6f785cff1813ae55ea5a23f92388125000
shakib <hello@shakib.tech>
2020-10-23 18:41:24 +0600

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,41 @@
 module.exports = {
     siteMetadata: {
         title: `বালের ব্লগ`,
-        description: `বিভিন্ন বালছাল, ট্রেন্ডিং বিষয়, হাবিজাবি ব্যাপারে এক্সপার্ট ব্যক্তিদের লিখালিখির স্থান`,
+        description: `বালছাল বিষয়ে নিত্য নতুন পোস্ট`,
         keywords: [`বাল`, `ব্লগ`, `বালের ব্লগ`, `বালছাল`, `ট্রেন্ডিং`, `হাবিজাবি`, `হযবরল`],
         author: `বালের অ্যাডমিন`,
         siteUrl: `https://baal.xyz`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
         menuLinks: [{
-                name: `বালের ব্লগে লিখুন`,
-                link: `/write-here`,
-                type: `internal`, //internal or anchor
-            },
-            {
-                name: `বালের যোগাযোগ`,
-                link: `/contact`,
-                type: `internal`, //internal or anchor
-            },
-        ],
+            name: `বালের ব্লগে লিখুন`,
+            link: `/write-here`,
+            type: `internal`, //internal or anchor
+        }, {
+            name: `বালের যোগাযোগ`,
+            link: `/contact`,
+            type: `internal`, //internal or anchor
+        }, ],
         socialLinks: [{
-                name: `Email`,
-                link: `ki@baal.xyz`,
-                location: `footer`, //Options are "all", "header", "footer"
-            },
-            {
-                name: `Facebook`,
-                link: `https://www.facebook.com/baal.xyz`,
-                location: `all`, //Options are "all", "header", "footer"
-            },
-            {
-                name: `Instagram`,
-                link: `https://www.instagram.com/baal.xyz`,
-                location: `all`, //Options are "all", "header", "footer"
-            },
-            {
-                name: `Twitter`,
-                link: `https://www.twitter.com/baal.xyz`,
-                location: `all`, //Options are "all", "header", "footer"
-            },
-        ],
+            name: `Email`,
+            link: `ki@baal.xyz`,
+            location: `footer`, //Options are "all", "header", "footer"
+        }, {
+            name: `Facebook`,
+            link: `https://www.facebook.com/baal.xyz`,
+            location: `all`, //Options are "all", "header", "footer"
+        }, {
+            name: `Instagram`,
+            link: `https://www.instagram.com/baal.xyz`,
+            location: `all`, //Options are "all", "header", "footer"
+        }, {
+            name: `Twitter`,
+            link: `https://www.twitter.com/baal.xyz`,
+            location: `all`, //Options are "all", "header", "footer"
+        }, ],
     },
     plugins: [
         `gatsby-plugin-netlify`,
         `gatsby-plugin-netlify-cms`,
-        `netlify-cms-app`,
-        {
+        `netlify-cms-app`, {
             resolve: `gatsby-theme-catalyst-helium`,
             options: {
                 // Core theme
@@ -56,8 +49,7 @@ module.exports = {
                 useHero: true,
                 useStickyHeader: true,
             },
-        },
-        {
+        }, {
             resolve: `gatsby-plugin-manifest`,
             options: {
                 name: `বালের ব্লগ`,
@@ -68,12 +60,16 @@ module.exports = {
                 display: `minimal-ui`,
                 icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
             },
-        },
-        {
+        }, {
             resolve: `gatsby-theme-catalyst-blog`,
             options: {
-                postListTitle: `বালের পোস্ট`,
+                postListTitle: `পোস্টসমূহ`,
                 useColorMode: `dark`,
+            },
+        }, {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+                trackingId: `G-X6VQKZ7NJ2`,
             },
         },
     ],
